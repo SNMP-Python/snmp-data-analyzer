6f837a50e79ce2911ae605dd85624f0f1d3c9234
Oscar <oscar13785@gmail.com>
2022-06-04 11:22:07 +0200

    feat(traps): adds trap script

---
+++ traps/trapsh.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+read host
+read ip
+vars=
+while read oid val
+do
+	if [ "$vars" = "" ]
+	then
+		vars="$oid = $val"
+	else
+		vars="$vars, $oid = $val"
+	fi
+done
+echo trap: $1 $host $ip $vars
