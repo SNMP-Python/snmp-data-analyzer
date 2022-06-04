06ea11b89fbd7a1e66da46f0a09584e7a8ffbe5f
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-06-04 19:35:37 +0200

    style: fix linter

--- parser/interface_remover.py
+++ parser/interface_remover.py
@@ -7,17 +7,19 @@ from parser.value_objects.router import Router
 
 
 class InterfaceRemover:
-
     @staticmethod
     def remove_down_and_loopback(routers: List[Router]):
-        InterfaceRemover._remove_interfaces(routers=routers,
-                                            filter_strategy=lambda interface: interface.status != InterfaceStatus.DOWN)
-        InterfaceRemover._remove_interfaces(routers=routers,
-                                            filter_strategy=lambda
-                                                interface: interface.type_interface != InterfaceType.LOOPBACK)
+        InterfaceRemover._remove_interfaces(
+            routers=routers, filter_strategy=lambda interface: interface.status != InterfaceStatus.DOWN
+        )
+        InterfaceRemover._remove_interfaces(
+            routers=routers, filter_strategy=lambda interface: interface.type_interface != InterfaceType.LOOPBACK
+        )
 
     @staticmethod
     def _remove_interfaces(filter_strategy: Callable[[Interface], bool], routers):
         for router in routers:
-            new_interfaces: List[Interface] = [interface for interface in router.interfaces if filter_strategy(interface)]
+            new_interfaces: List[Interface] = [
+                interface for interface in router.interfaces if filter_strategy(interface)
+            ]
             router.interfaces = new_interfaces
