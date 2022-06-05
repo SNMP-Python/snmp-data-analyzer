6db7644c697118869388805d88b425f11411c920
Oscar <oscar13785@gmail.com>
2022-06-05 12:25:59 +0200

    feat(traps): parser almost finished

--- traps/mibs_parser.py
+++ traps/mibs_parser.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-class StatesMIB(Enum):
+class OSPFStatesMIB(Enum):
     DOWN = ' SNMPv2-SMI::mib-2.14.10.1.6 = 1\n'
     ATTEMPT = ' SNMPv2-SMI::mib-2.14.10.1.6 = 2\n'
     INIT = ' SNMPv2-SMI::mib-2.14.10.1.6 = 3\n'
@@ -9,6 +9,15 @@ class StatesMIB(Enum):
     EXCHANGE = ' SNMPv2-SMI::mib-2.14.10.1.6 = 6\n'
     LOADING = ' SNMPv2-SMI::mib-2.14.10.1.6 = 7\n'
     FULL = ' SNMPv2-SMI::mib-2.14.10.1.6 = 8\n'
+
+class InterfaceStateMIB(Enum):
+    DOWN = ' SNMPv2-SMI::mib-2.14.7.1.12 = 1\n'
+    LOOPBACK = ' SNMPv2-SMI::mib-2.14.7.1.12 = 2\n'
+    WAITING = ' SNMPv2-SMI::mib-2.14.7.1.12 = 3\n'
+    POINT_TO_POINT = ' SNMPv2-SMI::mib-2.14.7.1.12 = 4\n'
+    DESIGNATED_ROUTER = ' SNMPv2-SMI::mib-2.14.7.1.12 = 5\n'
+    BACKUP_DESIGNATED_ROUTER = ' SNMPv2-SMI::mib-2.14.7.1.12 = 6\n'
+    OTHER_DESIGNATED_ROUTER = ' SNMPv2-SMI::mib-2.14.7.1.12 = 7\n'
 
 class InterfacesMIB(Enum):
     ROUTER_ID = ' SNMPv2-SMI::mib-2.14.1.1 '
--- traps/traps_parser.py
+++ traps/traps_parser.py
@@ -1,4 +1,4 @@
-from mibs_parser import StatesMIB, InterfacesMIB, MIBType
+from mibs_parser import OSPFStatesMIB, InterfacesMIB, MIBType, InterfaceStateMIB
 
 
 def get_header(events):
@@ -7,20 +7,38 @@ def get_header(events):
     return header, events
 
 event_switch = {
-    StatesMIB.DOWN.value: "STATE: FULL",
-    StatesMIB.ATTEMPT.value: "STATE: ATTEMPT",
-    StatesMIB.INIT.value: "STATE: INIT",
-    StatesMIB.TWO_WAY.value: "STATE: TWO_WAY",
-    StatesMIB.EXCHANGE_START.value: "STATE: EXCHANGE_START",
-    StatesMIB.EXCHANGE.value: "STATE: EXCHANGE",
-    StatesMIB.LOADING.value: "STATE: LOADING",
-    StatesMIB.FULL.value: "STATE: FULL",
+    OSPFStatesMIB.DOWN.value: "STATE: FULL",
+    OSPFStatesMIB.ATTEMPT.value: "STATE: ATTEMPT",
+    OSPFStatesMIB.INIT.value: "STATE: INIT",
+    OSPFStatesMIB.TWO_WAY.value: "STATE: TWO_WAY",
+    OSPFStatesMIB.EXCHANGE_START.value: "STATE: EXCHANGE_START",
+    OSPFStatesMIB.EXCHANGE.value: "STATE: EXCHANGE",
+    OSPFStatesMIB.LOADING.value: "STATE: LOADING",
+    OSPFStatesMIB.FULL.value: "STATE: FULL",
+    InterfaceStateMIB.DOWN.value: "INTERFACE STATE: DOWN",
+    InterfaceStateMIB.LOOPBACK.value: "INTERFACE STATE: LOOPBACK",
+    InterfaceStateMIB.WAITING.value: "INTERFACE STATE: WAITING",
+    InterfaceStateMIB.POINT_TO_POINT.value: "INTERFACE STATE: POINT_TO_POINT",
+    InterfaceStateMIB.DESIGNATED_ROUTER.value: "INTERFACE STATE: DESIGNATED_ROUTER",
+    InterfaceStateMIB.BACKUP_DESIGNATED_ROUTER.value: "INTERFACE STATE: BACKUP_DESIGNATED_ROUTER",
+    InterfaceStateMIB.OTHER_DESIGNATED_ROUTER   .value: "INTERFACE STATE: OTHER_DESIGNATED_ROUTER",
 }
 
 mib_type_switch = {
     MIBType.IF_STATE_CHANGE.value: "IF STATE CHANGE",
     MIBType.NBR_STATE_CHANGE.value: "NEIGHBOR STATE CHANGE"
 }
+
+interfaces_mib_switch = {
+    InterfacesMIB.ROUTER_ID.value: ' SNMPv2-SMI::mib-2.14.1.1 ',
+    InterfacesMIB.INTERFACE_IP.value: ' SNMPv2-SMI::mib-2.14.7.1.1 '
+}
+
+def get_and_print_ids(event):
+    if event.split('=')[0] == ' SNMPv2-SMI::mib-2.14.1.1 ':
+        print('ROUTER ID: ' + event.split('=')[1] )
+    if event.split('=')[0] == ' SNMPv2-SMI::mib-2.14.7.1.1 ':
+        print('INTERFACE ID: ' + event.split('=')[1] )
 
 def parse_line(line):
     events = line.split(',')
@@ -29,6 +47,7 @@ def parse_line(line):
         print('----------------------------------------------------')
         print(mib_type_switch.get(events[0]))
         for event in events:
+            get_and_print_ids(event)
             print(event_switch.get(event,""), end="")
 
         print()
