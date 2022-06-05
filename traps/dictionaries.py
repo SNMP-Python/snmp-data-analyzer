6f2622fe03fd8ed8b5a8cc58d9592d636df0489d
Oscar <oscar13785@gmail.com>
2022-06-05 12:32:24 +0200

    feat(traps): refactored traps_parser.py

---
+++ traps/dictionaries.py
@@ -0,0 +1,29 @@
+from states_enums import OSPFStatesMIB, InterfaceStateMIB, MIBType, InterfacesMIB
+
+event_switch = {
+    OSPFStatesMIB.DOWN.value: "OSPF ROUTER STATE: FULL",
+    OSPFStatesMIB.ATTEMPT.value: "OSPF ROUTER STATE: ATTEMPT",
+    OSPFStatesMIB.INIT.value: "OSPF ROUTER STATE: INIT",
+    OSPFStatesMIB.TWO_WAY.value: "OSPF ROUTER STATE: TWO_WAY",
+    OSPFStatesMIB.EXCHANGE_START.value: "OSPF ROUTER STATE: EXCHANGE_START",
+    OSPFStatesMIB.EXCHANGE.value: "OSPF ROUTER STATE: EXCHANGE",
+    OSPFStatesMIB.LOADING.value: "OSPF ROUTER STATE: LOADING",
+    OSPFStatesMIB.FULL.value: "OSPF ROUTER STATE: FULL",
+    InterfaceStateMIB.DOWN.value: "INTERFACE STATE: DOWN",
+    InterfaceStateMIB.LOOPBACK.value: "INTERFACE STATE: LOOPBACK",
+    InterfaceStateMIB.WAITING.value: "INTERFACE STATE: WAITING",
+    InterfaceStateMIB.POINT_TO_POINT.value: "INTERFACE STATE: POINT_TO_POINT",
+    InterfaceStateMIB.DESIGNATED_ROUTER.value: "INTERFACE STATE: DESIGNATED_ROUTER",
+    InterfaceStateMIB.BACKUP_DESIGNATED_ROUTER.value: "INTERFACE STATE: BACKUP_DESIGNATED_ROUTER",
+    InterfaceStateMIB.OTHER_DESIGNATED_ROUTER   .value: "INTERFACE STATE: OTHER_DESIGNATED_ROUTER",
+}
+
+mib_type_switch = {
+    MIBType.IF_STATE_CHANGE.value: "IF STATE CHANGE",
+    MIBType.NBR_STATE_CHANGE.value: "NEIGHBOR STATE CHANGE"
+}
+
+interfaces_mib_switch = {
+    InterfacesMIB.ROUTER_ID.value: ' SNMPv2-SMI::mib-2.14.1.1 ',
+    InterfacesMIB.INTERFACE_IP.value: ' SNMPv2-SMI::mib-2.14.7.1.1 '
+}
--- traps/traps_parser.py
+++ traps/traps_parser.py
@@ -1,38 +1,18 @@
-from mibs_parser import OSPFStatesMIB, InterfacesMIB, MIBType, InterfaceStateMIB
+from dictionaries import mib_type_switch, event_switch
 
+def print_div():
+    print('----------------------------------------------------')
+
+def print_event_type(events):
+    print(mib_type_switch.get(events[0]))
+
+def print_if_or_router_state(event):
+    print(event_switch.get(event, ""), end="")
 
 def get_header(events):
     header = events[0]
     events = events[1:]
     return header, events
-
-event_switch = {
-    OSPFStatesMIB.DOWN.value: "STATE: FULL",
-    OSPFStatesMIB.ATTEMPT.value: "STATE: ATTEMPT",
-    OSPFStatesMIB.INIT.value: "STATE: INIT",
-    OSPFStatesMIB.TWO_WAY.value: "STATE: TWO_WAY",
-    OSPFStatesMIB.EXCHANGE_START.value: "STATE: EXCHANGE_START",
-    OSPFStatesMIB.EXCHANGE.value: "STATE: EXCHANGE",
-    OSPFStatesMIB.LOADING.value: "STATE: LOADING",
-    OSPFStatesMIB.FULL.value: "STATE: FULL",
-    InterfaceStateMIB.DOWN.value: "INTERFACE STATE: DOWN",
-    InterfaceStateMIB.LOOPBACK.value: "INTERFACE STATE: LOOPBACK",
-    InterfaceStateMIB.WAITING.value: "INTERFACE STATE: WAITING",
-    InterfaceStateMIB.POINT_TO_POINT.value: "INTERFACE STATE: POINT_TO_POINT",
-    InterfaceStateMIB.DESIGNATED_ROUTER.value: "INTERFACE STATE: DESIGNATED_ROUTER",
-    InterfaceStateMIB.BACKUP_DESIGNATED_ROUTER.value: "INTERFACE STATE: BACKUP_DESIGNATED_ROUTER",
-    InterfaceStateMIB.OTHER_DESIGNATED_ROUTER   .value: "INTERFACE STATE: OTHER_DESIGNATED_ROUTER",
-}
-
-mib_type_switch = {
-    MIBType.IF_STATE_CHANGE.value: "IF STATE CHANGE",
-    MIBType.NBR_STATE_CHANGE.value: "NEIGHBOR STATE CHANGE"
-}
-
-interfaces_mib_switch = {
-    InterfacesMIB.ROUTER_ID.value: ' SNMPv2-SMI::mib-2.14.1.1 ',
-    InterfacesMIB.INTERFACE_IP.value: ' SNMPv2-SMI::mib-2.14.7.1.1 '
-}
 
 def get_and_print_ids(event):
     if event.split('=')[0] == ' SNMPv2-SMI::mib-2.14.1.1 ':
@@ -44,17 +24,20 @@ def parse_line(line):
     events = line.split(',')
     header, events = get_header(events)
     if events:
-        print('----------------------------------------------------')
-        print(mib_type_switch.get(events[0]))
+        print_div()
+        print_event_type(events)
         for event in events:
             get_and_print_ids(event)
-            print(event_switch.get(event,""), end="")
+            print_if_or_router_state(event)
 
         print()
-        print('----------------------------------------------------')
 
 if __name__ == "__main__":
     file = open('logs.txt')
     for line in file:
         parse_line(line)
+
+    print_div()
+
+
 
