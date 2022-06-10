0f7a4b14d1783c5794a79ce0459d871522df366c
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-06-10 12:24:31 +0200

    fix: queries on cisco 15.x routers

--- searcher/pollers/routing_table_poller.py
+++ searcher/pollers/routing_table_poller.py
@@ -1,53 +1,37 @@
 from __future__ import absolute_import
 
-from typing import List
+from typing import List, Tuple
 
 from easysnmp import Session
 
-from searcher.exceptions.next_hop_not_found_exception import NextHopNotFoundException
 from searcher.pollers.poller import Poll
 from searcher.primitives.route_primitives import RoutePrimitives
 
 ROUTE_NETWORK_OID = "IP-FORWARD-MIB::ipCidrRouteDest"
-ROUTE_MASK_OID = "RFC1213-MIB::ipRouteMask"
+ROUTE_MASK_OID = "IP-FORWARD-MIB::ipCidrRouteMask"
 ROUTE_NEXT_HOP_OID = "IP-FORWARD-MIB::ipCidrRouteNextHop"
 ROUTE_TYPE_OID = "IP-FORWARD-MIB::ipCidrRouteType"
 
 
 class RoutingTablePoller(Poll):
     def poll(self) -> List[RoutePrimitives]:
-        return list(
-            map(
-                lambda network: RoutingTablePoller._get_route_from_network(self.session, network),
-                RoutingTablePoller._get_networks(self.session),
-            )
-        )
+        primitives = RoutingTablePoller.poll_everything(self.session)
+        return self.get_list_route_primitives(primitives)
 
     @staticmethod
-    def _get_networks(session: Session) -> List[str]:
-        return list(map(lambda entry: str(entry.value), session.walk(ROUTE_NETWORK_OID)))
+    def poll_everything(session: Session) -> Tuple[List[str], List[str], List[str], List[str]]:
+        networks = list(map(lambda x: x.value, session.walk(ROUTE_NETWORK_OID)))
+        masks = list(map(lambda x: x.value, session.walk(ROUTE_MASK_OID)))
+        next_hop = list(map(lambda x: x.value, session.walk(ROUTE_NEXT_HOP_OID)))
+        type_link = list(map(lambda x: x.value, session.walk(ROUTE_TYPE_OID)))
+        return networks, masks, next_hop, type_link
 
-    @staticmethod
-    def _get_route_from_network(session: Session, network: str) -> RoutePrimitives:
-        mask = RoutingTablePoller._get_route_mask(session, network)
-        return RoutePrimitives(
-            network=network,
-            mask=mask,
-            next_hop=RoutingTablePoller._get_route_next_hop(session, network, mask),
-            route_type=RoutingTablePoller._get_route_type(session, network, mask),
-        )
-
-    @staticmethod
-    def _get_route_mask(session: Session, network: str) -> str:
-        return str(session.get(ROUTE_MASK_OID + "." + network).value)
-
-    @staticmethod
-    def _get_route_next_hop(session: Session, network: str, mask: str) -> str:
-        result = session.walk(ROUTE_NEXT_HOP_OID + "." + network + "." + mask)
-        if len(result) == 0:
-            raise NextHopNotFoundException()
-        return str(result[0].value)
-
-    @staticmethod
-    def _get_route_type(session: Session, network: str, mask: str) -> str:
-        return str(session.walk(ROUTE_TYPE_OID + "." + network + "." + mask)[0].value)
+    def get_list_route_primitives(self, primitives: Tuple[List[str], List[str], List[str], List[str]]) -> List[RoutePrimitives]:
+        networks = primitives[0]
+        masks = primitives[1]
+        next_hops = primitives[2]
+        type_link = primitives[3]
+        result = []
+        for i in range(0, len(networks)):
+            result.append(RoutePrimitives(networks[i], masks[i], next_hops[i], type_link[i]))
+        return result
